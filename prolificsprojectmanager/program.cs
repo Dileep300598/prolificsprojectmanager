e6e8357b1fbb21f8486466caff02e19bcdfee9a9
Dileep300598 <dileepkumar4202@gmail.com>
2021-09-21 18:12:24 +0530

    ..

---
+++ prolificsprojectmanager/program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using ppm.model;
+using ppm.ui.cli;
+
+namespace ppm 
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            if (args is null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+            cmd co = new cmd();
+            co.startprogram();
+
+        }
+    }
+}
+
+

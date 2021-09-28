75edc9e70c44d2a052f1ea866288e8339aa79823
Dileep300598 <dileepkumar4202@gmail.com>
2021-09-28 18:36:30 +0530

    3.3

---
+++ ppm test/UnitTest1.cs
@@ -0,0 +1,120 @@
+using Domain;
+using NUnit.Framework;
+using ppm.domain;
+using ppm.model;
+using System;
+
+namespace ppm_test
+{
+    public class Tests
+    {
+
+
+        [Test]
+        public void AddProjectTest()
+        {
+            Projectmanager project = new Projectmanager();
+            Project pp = new Project();
+            pp.id = 1;
+            pp.Name = "Dileep";
+            pp.StartDate = Convert.ToDateTime("01 - 01 - 2020");
+            pp.EndDate = Convert.ToDateTime("01-01-2022");
+            pp.Budget = 400000;
+            var v1 = project.AddProject(pp);
+            if (v1.isSucess)
+            {
+                Assert.Pass();
+            }
+            else
+            {
+                Assert.Fail();
+            }
+
+        }
+        [Test]
+        public void AddEmployeeTest()
+        {
+            Employeemanager employee = new Employeemanager();
+            Employee ee = new Employee();
+            ee.Id = 1;
+            ee.Fullname = "Dileep";
+            ee.Contact = 123456;
+            ee.RoleName = "Hr";
+            var e1 = employee.AddEmployee(ee);
+            if (e1.isSucess)
+            {
+                Assert.Pass();
+            }
+            else
+            {
+                Assert.Fail();
+            }
+        }
+        [Test]
+        public void AddRoleTest()
+        {
+            Rolemanager role = new Rolemanager();
+            Role r = new Role();
+            r.RoleId = 12;
+            r.RoleName = "Manager";
+            var r1 = role.AddRole(r);
+            if (r1.isSucess)
+            {
+                Assert.Pass();
+            }
+            else
+            {
+                Assert.Fail();
+            }
+
+        }
+       [Test]
+       public void AddEmployeeToProject()
+        {
+            Employee emp = new Employee();
+            Employeemanager ee = new Employeemanager();
+            int id = 1;
+            emp.Fullname = "dileep";
+            var v1 = ee.IsValidEmp(emp);
+            if (!v1.isSucess)
+            {
+                Projectmanager projectmanager = new Projectmanager();
+                var r1 = projectmanager.AddEmpToProject(emp, id);
+                if (!r1.isSucess) 
+                {
+                    Assert.Pass();
+                }
+                else
+                {
+                    Assert.Fail();
+                }
+               
+            }
+
+        }
+        [Test]
+        public void RemoveEmployeeFromProject()
+        {
+            Employee emp = new Employee();
+            Employeemanager ee = new Employeemanager();
+            int id = 1;
+            emp.Fullname = "dileep";
+            var e1 = ee.IsValidEmp(emp);
+            if (e1.isSucess)
+            {
+                Projectmanager projectmanager = new Projectmanager();
+                var r1 = projectmanager.DeleteEmpFromProject(id , emp);
+                if (!r1.isSucess)
+                {
+                    Assert.Pass();
+                }
+                else
+                {
+                    Assert.Fail();
+                }
+
+            }
+
+        }
+    }
+}
